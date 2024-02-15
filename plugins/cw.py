adaf5445121210b435eda830c1967909f152c30d
Ekvillai9 <158746555+Ekvillai9@users.noreply.github.com>
2024-02-15 19:03:07 +0530

    Update cw.py

--- plugins/cw.py
+++ plugins/cw.py
@@ -103,8 +103,8 @@ async def account_login(bot: Client, m: Message):
           await m.reply_text(token)
       else:
           await m.reply_text("go back to response")
-  else:
-      token = raw_text
+    else:
+         token = raw_text
       if token:
       html1 = s.get("https://elearn.crwilladmin.com/api/v5/my-batch?&token=" + token).json()
       topicid = html1["data"]["batchData"]
