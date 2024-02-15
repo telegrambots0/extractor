4f8db469df8a46d59145ec65f91514f5aecbe764
Ekvillai9 <158746555+Ekvillai9@users.noreply.github.com>
2024-02-15 18:22:48 +0530

    Update cw.py

--- plugins/cw.py
+++ plugins/cw.py
@@ -97,15 +97,15 @@ async def account_login(bot: Client, m: Message):
       await input1.delete(True)
       #s = requests.Session()
       response = s.post(url = url, headers=headers, json=data, timeout=10)
-      if response.status_code == 200:
+    if response.status_code == 200:
           data = response.json()
           token = data["data"]["token"]
           await m.reply_text(token)
-      else:
+    else:
           await m.reply_text("go back to response")
-      else:
-         token = raw_text
-      if token:
+    else:
+    token = raw_text
+    if token:
       html1 = s.get("https://elearn.crwilladmin.com/api/v5/my-batch?&token=" + token).json()
       topicid = html1["data"]["batchData"]
       cool=""
