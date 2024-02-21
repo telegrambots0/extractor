b64d0ec8e8cf86560f9c521efd397a645b5d8e3b
Ekvillai9 <158746555+Ekvillai9@users.noreply.github.com>
2024-02-22 00:23:05 +0530

    Update cw.py

--- plugins/cw.py
+++ plugins/cw.py
@@ -81,11 +81,11 @@ async def account_login(bot: Client, m: Message):
         "user-agent": "okhttp/5.0.0-alpha",
         'Connection': 'Keep-Alive'
        }
-     proxy_host = ['104.26.3.116']
-     proxies = {
-             'https': proxy_host,
-             'http': proxy_host,
-         }
+       proxy_host = ['104.26.3.116']
+       proxies = {
+           'https': proxy_host,
+           'http': proxy_host,
+       }
     editable = await m.reply_text("Send **ID & Password** in this manner otherwise bot will not respond.\n\nSend like this:-  **ID*Password** \n or \nSend **TOKEN** like This this:-  **TOKEN**" )
     input1: Message = await bot.listen(editable.chat.id)
     raw_text = input1.text
